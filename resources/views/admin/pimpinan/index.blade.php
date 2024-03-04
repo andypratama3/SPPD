d46e4b4eda34d5343f3cb2b0c1e51116a49b503c
DX-STRO <aldinnuradzin@gmail.com>
2024-03-04 21:24:42 +0800

    pimpinan/tambah-button

--- resources/views/admin/pimpinan/index.blade.php
+++ resources/views/admin/pimpinan/index.blade.php
@@ -7,7 +7,7 @@
             <div class="card-body">
                 <div class="form-group">
                     <h4 class="text-center">Pimpinan</h4>
-                    <a href="{{ route('dashboard.pimpinan.create') }}" class="btn btn-primary btn-rounded btn-fw mb-4" style="float: inline-end; margin-right: 14px;"><i class="fas fa-plus"></i> Tambah</a>
+                    <a href="{{ route('dashboard.pimpinan.create') }}" class="btn btn-primary btn-rounded btn-fw mb-4" style="float: inline-end; margin-right: 14px; color: white; padding: 12px;"><i class="fas fa-plus" style="font-size: 12px"></i> Tambah</a>
                 </div>
                 <table class="table table-responsive text-center" id="table_pimpinan">
                     <tr>
@@ -30,7 +30,7 @@
                             <td>
                                 <a href="{{ route('dashboard.pimpinan.show', $pimpinan->slug) }}" class="btn btn-secondary btn-xs"><i class="fas fa-eye"></i></a>
                                 <a href="{{ route('dashboard.pimpinan.edit', $pimpinan->slug) }}" class="btn btn-warning btn-xs"><i class="fas fa-pen"></i></a>
-                                <a href="" class="btn btn-danger btn-xs"><i class="fas fa-trash"></i></a>
+                                <a href="" class="btn btn-danger btn-xs" style="color: white;"><i class="fas fa-trash"></i></a>
                             </td>
                         </tr>
                         @endforeach
